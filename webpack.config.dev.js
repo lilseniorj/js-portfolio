d89e08cacf84d487676c4a293acc0fde9dacb42b
Jesus Vargas <jesusvarguer18@gmail.com>
2022-10-27 17:51:59 -0500

    LocalHost

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,6 @@ module.exports = {
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   module: 'development',
-  watch: true,
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -77,4 +76,13 @@ module.exports = {
     }),
     new Dotenv(),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+		historyApiFallback: true,
+		port: 8080,
+		open: true,
+  },
 }
