e7b4a42353e3d79f40abf0ce739e7b701261f67b
Jesus Vargas <jesusvarguer18@gmail.com>
2022-11-02 19:07:43 -0500

    devtools

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,15 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[constenhash].js',
+    filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
-  module: 'development',
+  mode: 'development',
+  devtool: 'source-map',
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -31,15 +33,15 @@ module.exports = {
         }
       },
       {
-        test: /\.s?css|.styl$/,
+        test: /\.css|.styl$/i,
         use: [MiniCssExtractPlugin.loader,
           'css-loader',
-          'stylus-loader',
+          'stylus-loader'
         ],
       },
       {
         test: /\.png/,
-        type: "asset/resource"
+        type: 'asset/resource'
       },
       {
         test: /\.(woff|woff2)$/,
@@ -47,12 +49,12 @@ module.exports = {
           loader: 'url-loader',
           options: {
             limit: 10000,
-            mimetype: 'aplication/font-woff',
-            name: "[name].[constenthash].[ext]",
+            mimetype: "application/font-woff",
+            name: "[name].[contenthash].[ext]",
             outputPath: "./assets/fonts/",
             publicPath: "../assets/fonts/",
             esModule: false,
-          }
+          },
         }
       }
     ]
@@ -75,6 +77,7 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     static: {
